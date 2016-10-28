aa28a2ab145f6f3832590ba585b8e2c95aacdbd2
Luciano <lucianocfernandes@gmail.com>
2016-10-28 13:15:12 -0200

    First verifier thats a helper to make easier unit test assertations.

---
+++ PowerUp/UnitTest/Verifiers/PrimitiveVerifiers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpTestsEx;
+
+namespace PowerUp
+{
+    public static class PrimitiveVerifiers
+    {
+        public static void ShouldBeToday(this DateTime self)
+        {
+            self.Date.Should().Be(DateTime.Today);
+        }
+
+        public static void ShouldBeNull<T>(this T? self) where T : struct
+        {
+            self.HasValue.Should().Be.False();
+        }
+
+        public static void ShouldBeNotNull<T>(this T? self) where T : struct
+        {
+            self.HasValue.Should().Be.True();
+        }
+
+    }
+}
