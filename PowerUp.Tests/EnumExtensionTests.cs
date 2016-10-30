26fd17131620690c4c5b9bda4c6e874a98bee215
Luciano <lucianocfernandes@gmail.com>
2016-10-30 00:00:46 -0200

    New: extension for enums to allow define a custom display for enum element.

---
+++ PowerUp.Tests/EnumExtensionTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpTestsEx;
+using System.ComponentModel.DataAnnotations;
+
+namespace PowerUp.Tests
+{
+    [TestClass]
+    public class EnumExtensionsTests
+    {
+        [TestMethod]
+        public void ShouldBePossibleToSplitCamelCaseInBlankSpaces()
+        {
+            EnumSample.SomeEnumElement.Display().Should().Be("Some Enum Element");
+        }
+
+        [TestMethod]
+        public void ShouldBePossibleToGetEnumElementDescription()
+        {
+            EnumSample.SomeDecoratedEnumElement.Display().Should().Be("My custom display");
+        }
+
+        private void VerifyQuarter(IEnumerable<string> foundMonths, params object[] expectedMonths)
+        {
+            var index = 0;
+            foreach(var expectedMonth in expectedMonths)
+            {
+                foundMonths.ElementAt(index).Should().Be(expectedMonth.ToString());
+                index += 1;
+            }
+        }
+    }
+
+    public enum EnumSample
+    {
+        SomeEnumElement,
+        [DisplayAs("My custom display")]
+        SomeDecoratedEnumElement
+    }
+}
---
+++ PowerUp/Extensions/EnumExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace PowerUp
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get enum element display.
+        /// </summary>
+        /// <param name="self">Enum element</param>
+        /// <returns>
+        /// If DisplayAsAttribute is present returns custom display of enum element, 
+        /// otherwise returns enum element name with blank space before every capital letter.
+        /// </returns>
+        public static string Display(this Enum self)
+        {
+            var enumValue = self.ToString();
+            var displayAs = self.GetType().GetMember(enumValue).First().GetCustomAttribute<DisplayAsAttribute>();
+            return displayAs != null ? displayAs.Value : Regex.Replace(enumValue, @"([A-Z])", " $1").Trim();
+        }
+
+        public static string ToString(this Enum self) 
+        {
+            return Display(self);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class DisplayAsAttribute: Attribute
+    {
+        public string Value { get; set; }
+
+        public DisplayAsAttribute(string display)
+        {
+            Value = display;
+        }
+    }
+}
