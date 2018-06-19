a1914416230b81721a5c6c7882f0809c52859b60
Luciano <lucianocfernandes@gmail.com>
2018-06-19 19:29:00 -0300

    Added support to SendGrid

--- PowerUp.Mvc.Security/Properties/AssemblyInfo.cs
+++ PowerUp.Mvc.Security/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PowerUp.Mvc")]
+[assembly: AssemblyTitle("PowerUp.Mvc.Security")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PowerUp.Mvc")]
+[assembly: AssemblyProduct("PowerUp.Mvc.Security")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- PowerUp.Mvc.Security/Security/EmailIdentityService.cs
+++ PowerUp.Mvc.Security/Security/EmailIdentityService.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNet.Identity;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,13 @@ namespace PowerUp.Mvc.Security
         public Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
+            var sendGridApiKey = ConfigurationManager.AppSettings["SendGridApiKey"].ToString();
+            var sendGridSender = ConfigurationManager.AppSettings["SendGridMailFrom"].ToString();
+            dynamic sg = new SendGridAPIClient(sendGridApiKey);
+            var body = new Content("text/plain", message.Body);
+            var mail = new Mail(new Email(sendGridSender), message.Subject, new Email(message.Destination), body);
+            dynamic response = sg.client.mail.send.post(requestBody: mail.Get());
+            
             return Task.FromResult(0);
         }
     }
